626aabd9add69b5f14a1a79a770fdae0a94d6910
baemir <baemir.omurzak0v@mail.ru>
2025-07-17 17:44:17 +0600

    Были добавлены шаблоны

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-] 
+    path("car_detail/<int:car_id>", views.car_detail, name="car_detail"),
+    path("passenger_cars", views.passenger_cars, name="passenger_cars"),
+    path("moto_cars", views.moto_cars, name="moto_cars"),
+    path("commercial_cars", views.commercial_cars, name="commercial_cars"),
+    ] 
--- main/views.py
+++ main/views.py
@@ -6,3 +6,27 @@ def index(request):
     """Главная страница."""
     cars = Car.objects.all()
     return render(request, 'main/index.html', {'cars': cars})
+
+
+def car_detail(request, car_id):
+    """Подробнее о машине."""
+    car = Car.objects.get(id=car_id)
+    return render(request, 'main/car_detail.html', {'car': car})
+
+
+def passenger_cars(request):
+    """Легковые автомобили."""
+    cars = Car.objects.filter(category__name='Легковые')
+    return render(request, 'main/passenger_cars.html', {'cars': cars})
+
+
+def moto_cars(request):
+    """Мотоциклы."""
+    cars = Car.objects.filter(category__name='Мото')
+    return render(request, 'main/moto.html', {'cars': cars})
+
+
+def commercial_cars(request):
+    """Коммерческие автомобили."""
+    cars = Car.objects.filter(category__name='Коммерческие')
+    return render(request, 'main/commercial.html', {'cars': cars})
